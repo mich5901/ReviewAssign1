d7c497a16e3fad28f332a8dd3716324398438e83
mich5901 <mich5901@ISS2V4PKH2.scdsb.pri>
2020-09-15 12:09:38 -0400

    Initial Commit

--- src/PizzaCost1.java
+++ src/PizzaCost1.java
@@ -1,8 +1,13 @@
 
+import java.util.Scanner;
+
+
 public class PizzaCost1 {
 
     public static void main(String[] args) {
-
+    Scanner s = new Scanner(System.in);
+    final double labour = 1.5;
+    final double heat = 0.75;
     }
     
 }
