e536b3152acc89d3e00c4f0ab8cc521f695e38bc
mich5901 <mich5901@ISS2V4PKH2.scdsb.pri>
2020-09-15 12:34:40 -0400

    Initial Commit

--- src/PizzaCost1.java
+++ src/PizzaCost1.java
@@ -8,6 +8,15 @@ public class PizzaCost1 {
     Scanner s = new Scanner(System.in);
     final double labour = 1.5;
     final double heat = 0.75;
+    final double toppings = 0.75;
+    final double ingredients = 0.50;
+    double inches, ingredientcost;
+    final double totalcost;
+    
+        System.out.print("Enter how many inches you would like your pizza to be > ");
+        inches = s.nextDouble();
+        ingredientcost = inches * ingredients;
+
     }
     
 }
