ee462d464a9cd266ed95c7aa1523a7707c812439
mich5901 <mich5901@ISS2V4PKH2.scdsb.pri>
2020-09-16 12:30:04 -0400

    Day 1 Initial Commit

--- src/BarChart.java
+++ src/BarChart.java
@@ -1,9 +1,22 @@
+
 import TurtleGraphics.*;
+import java.util.Scanner;
 
 public class BarChart {
 
     public static void main(String[] args) {
-    
+        Scanner s = new Scanner(System.in);
+        int a,b,c,d;
+        Pen p = new StandardPen();
+        SketchPadWindow window = new SketchPadWindow(800, 600);
+        
+        System.out.print("Enter your first value > ");
+        int xloc1 = s.nextInt();
+        System.out.print("Enter your second value > ");
+        int xloc2 = s.nextInt();
+        System.out.print("Enter your third value > ");
+        int xloc3 = s.nextInt();
+
     }
-    
+
 }
