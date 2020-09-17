3a1a60175636a6db34a79e669586723ec945de20
mich5901 <mich5901@ISS2V4PKH2.scdsb.pri>
2020-09-17 11:00:27 -0400

    Day 1 Initial Commit

--- src/part2/Divide.java
+++ src/part2/Divide.java
@@ -1,21 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package part2;
 
-/**
- *
- * @author mich5901
- */
+import java.util.Scanner;
+
 public class Divide {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+        int int1, int2, quo, rem;
+        System.out.print("Enter your first value > ");
+        int1 = s.nextInt();
+        System.out.print("Enter your second value > ");
+        int2 = s.nextInt();
+
+        if (int1 < int2) {
+            rem = int1 % int2;
+            System.out.println("The remainder is > " + rem);
+
+        } else if (int2 < int1) {
+            rem = int2 % int1;
+            System.out.println("The remainder is > " + rem);
+        }
+
     }
-    
+
 }
