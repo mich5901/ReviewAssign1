27d9e8f9b30fe6b8a0f7494e257be5f5ff376e86
mich5901 <mich5901@ISS2V4PKH2.scdsb.pri>
2020-09-28 10:09:16 -0400

    Day 1 Initial Commit

--- src/part2/EmployeePay.java
+++ src/part2/EmployeePay.java
@@ -1,4 +1,3 @@
-
 package part2;
 
 import java.util.Scanner;
@@ -6,27 +5,25 @@ import java.util.Scanner;
 public class EmployeePay {
 
     public static void main(String[] args) {
-     Scanner s = new Scanner(System.in);
-     double wage = 0 , overtime = 0, total;
-     int hours = 0, overtimehours;
-    
-        System.out.print("Enter employee ones hours worked > ");
-        hours = s.nextInt();
-        
-        for (int i = 0; i < 10; i++) {
-        if(hours>=40){
-         wage = 22.50;
-         total = hours * wage;
-            System.out.println("Employee one earns > $ " + total);
-            break;
-        } 
-        else if(hours<=40){
-            wage = 15.00;
-            total = hours * wage;
-            System.out.println("Employee one earns > " + total);
-            break;
-        }
+        Scanner s = new Scanner(System.in);
+        double wage = 0, overtime = 0, total;
+        int hours = 0, overtimehours;
+
+        for (int i =1; i <= 5; i++) {
+            System.out.print("Enter employees "+i+" hours worked > ");
+            hours = s.nextInt();
+            if (hours >= 40) {
+                wage = 22.50;
+                total = hours * wage;
+                System.out.println("Employee "+i+" earns > $ " + total);
+                
+            } else if (hours <= 40) {
+                wage = 15.00;
+                total = hours * wage;
+                System.out.println("Employee "+i+" earns > " + total);
+                
+            }
         }
     }
-    
+
 }
