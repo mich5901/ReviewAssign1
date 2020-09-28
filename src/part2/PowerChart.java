921378f468657c6b41fc50e36d4acf6a0e0980ac
mich5901 <mich5901@ISS2V4PKH2.scdsb.pri>
2020-09-28 10:19:23 -0400

    Day 1 Initial Commit

--- src/part2/PowerChart.java
+++ src/part2/PowerChart.java
@@ -1,10 +1,23 @@
-
 package part2;
 
 public class PowerChart {
 
     public static void main(String[] args) {
-    
+
+        System.out.format("X\t ");
+        System.out.format("2^X\n");
+
+        System.out.format("=\t ");
+        System.out.format("===\n");
+
+        double power;
+
+        for (int i = 0; i <= 7; i++) {
+            
+            System.out.format(i+"\t ");
+            System.out.println(Math.pow(2,i) + "\n");
+            
+        }
     }
-    
+
 }
